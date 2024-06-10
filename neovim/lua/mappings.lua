0fb19cc9317fe294a4c309f6e818ec74fc822b3c
Daniel Saad <dsaad68@gmail.com>
2024-06-11 00:12:01 +0200

    ⌨️ Updated Mapping for Neotest

--- neovim/lua/mappings.lua
+++ neovim/lua/mappings.lua
@@ -6,10 +6,25 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
-map("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
-map("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
-map("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
+
+-- INFO: For moving the lines
+map("n", "<A-j>", ":m .+1<CR>==", {desc = "Move line up(n)"}) -- move line up(n)
+map("n", "<A-k>", ":m .-2<CR>==", {desc = "Move line down(n)"}) -- move line down(n)
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", {desc = "Move line up(v)"}) -- move line up(v)
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", {desc = "Move line down(v)"}) -- move line down(v)
 
 
+-- INFO: For Neotest
+map("n", "<leader>ts", ":Neotest summary<CR>", {desc = "Toggle Neotest Summary"})
+map("n", "<leader>to", ":Neotest output<CR>", {desc = "Toggle Neotest Output"})
+map("n", "<leader>tp", ":Neotest output-panel toggle<CR>", {desc = "Toggle Neotest Output Pannel"})
+map("n", "<leader>tr", ":Neotest run<CR>", {desc = "Run nearest test with Neotest"})
+map("n", "<leader>tf", ":Neotest run file<CR>", {desc = "Run all tests in file with Neotest"})
+map("n", "<leader>tc", ":Neotest stop<CR>", {desc = "Stop Neotest"})
+map("n", "<leader>td", function() require("neotest").run.run({strategy = "dap"}) end, {desc = "Run nearest test with DAP & Neotest"} )
+map("n", "<leader>ta", function() require("neotest").run.attach() end, {desc = "Attach to nearest test with Neotest"} )
+-- map("n", "<leader>tot",  function() require("neotest").output.open({enter = true}) end, {desc = "Toggle Neotest Output for a test"})
+
+
+-- map("n", "<A-S>", ":Neotest summary<CR>", {desc = "Toggle Neotest Summary"})
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
