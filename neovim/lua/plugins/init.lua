72bab913c22bdd0f0908caad462d75d5d5d7829f
Daniel Saad <dsaad68@gmail.com>
2024-06-10 20:31:27 +0200

    ➕ Added Noice ➕ Added Precognition ➕ Added Rustaceanvim

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -26,6 +26,7 @@ return {
         "css-lsp",
         "prettier",
         "pyright",
+        "mypy",
         "ruff",
         "ruff-lsp",
         "rust-analyzer",
@@ -71,40 +72,90 @@ return {
   },
 
   {
-  "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
-  cmd = "Trouble",
-  keys = {
-    {
-      "<leader>xx",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
     },
+
     {
-      "<leader>xX",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+      },
     },
+
     {
-      "<leader>cs",
-      "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Symbols (Trouble)",
+      "mrcjkb/rustaceanvim",
+      version = "^4", -- Recommended
+      lazy = false, -- This plugin is already lazy
     },
+
     {
-      "<leader>cl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
-    },
-    {
-      "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
+      "tris203/precognition.nvim",
+      event = "VeryLazy",
+      config = {
+        startVisible = true,
+        showBlankVirtLine = true,
+        highlightColor = {fg = '#f72238'},
+        hints = {
+             Caret = { text = "^", prio = 5 },
+             Dollar = { text = "$", prio = 1 },
+             MatchingPair = { text = "%", prio = 5 },
+             Zero = { text = "0", prio = 1 },
+             w = { text = "w", prio = 10 },
+             b = { text = "b", prio = 9 },
+             e = { text = "e", prio = 8 },
+             W = { text = "W", prio = 7 },
+             B = { text = "B", prio = 6 },
+             E = { text = "E", prio = 5 },
+        },
+        gutterHints = {
+            G = { text = "G", prio = 10 },
+            gg = { text = "gg", prio = 9 },
+            PrevParagraph = { text = "{", prio = 8 },
+            NextParagraph = { text = "}", prio = 8 },
+        },
+      },
     },
   },
-}
 }
